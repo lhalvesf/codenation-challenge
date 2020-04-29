cb9175cabf53a895fd200502ff69af4c3e097d1b
Luiz <lh.alvesf@gmail.com>
2020-04-29 11:06:10 -0300

    ajustes

--- desafio.js
+++ desafio.js
@@ -4,19 +4,18 @@ var answer = JSON.parse(fs.readFileSync("answer.json"))
 const updateJsonFile = require('update-json-file')
 const filePath = 'answer.json'
 const sha1 = require('sha1')
-var FormData = require('form-data');
- 
+
 var letters = {
     caracteres: answer.cifrado.split(''),
     indexes: [],
     indiceOriginal: function(a,c) {
         this.caracteres.forEach(i => {
             if ( a.indexOf(i) != -1 ) {
-                let index = a.indexOf(i) + c
+                let index = a.indexOf(i) - c
                 if (index < 26 && index >= 0 ) {
                     this.indexes.push(a[index])
-                } else if ( index >= 26) {
-                    this.indexes.push(a[index-26])
+                } else if ( index < 0) {
+                    this.indexes.push(a[index + 26])
                 }            
             } else {
                 this.indexes.push(' ')
@@ -31,3 +30,6 @@ updateJsonFile(filePath, (data) => {
     data.resumo_criptografico = sha1(letters.indiceOriginal(alfabeto, 5))
     return data
 })
+
+
+console.log( sha1('the best engineers i know are artists at heart. the best designers i know are secretly technicians as well. andrei herasimchuk') )
